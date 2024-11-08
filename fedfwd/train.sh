58e099dd1837e5aa85e3a8cae37dc5018abfc433
LZhengguo <768153997@qq.com>
2024-11-08 10:16:32 +0800

    update again

--- fedfwd/train.sh
+++ fedfwd/train.sh
@@ -15,9 +15,10 @@
 # fwd test
 python fwd_main_tc.py  --pretrained_dir checkpoints/imagenet21k_ViT-B_16.npz --model_type ViT-B_16 \
           --n_parties 4 --cls_num 10 --device cuda:3 \
-         --batch_size 20 --comm_round 40  --test_round 30 --sample 1 --alg SGPT\
-        --dataset office --lr 0.01 --epochs 100 \
-        --peftmode adapter --check_layer_id 20 --fwdtrain
+         --batch_size 5 --comm_round 70  --test_round 30 --sample 1 --alg SGPT\
+        --dataset office --lr 0.01 --epochs 1 \
+        --peftmode adapter --check_layer_id 20 --fwdtrain_param --var_threshold 0.1 > logs/train.log
+# --bpfirst
 
 # python fwd_main_tc.py  --pretrained_dir checkpoints/imagenet21k_ViT-B_16.npz --model_type ViT-B_16 \
 #           --n_parties 4 --cls_num 10 --device cuda:3 \
